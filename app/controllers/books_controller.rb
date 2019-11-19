6b749361386e55aba8918dbca17595a17848980b
shunsuke sasaki <shunsukesasaki@shunsukenoMacBook-Pro.local>
2019-11-19 17:58:06 +0900

    [Add] 編集、削除機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,14 @@
 class BooksController < ApplicationController
 	def top
 	end
+
 	def about
 	end
+
 	def new
 		@book = Book.new
 	end
+
 	def create
 		@book = Book.new(book_params)
 		@book.user = current_user
@@ -15,10 +18,33 @@ class BooksController < ApplicationController
 			render "books/new"
 		end
 	end
+
 	def index
 		@books = Book.all
 	end
+
 	def show
+		@book =Book.find(params[:id])
+	end
+
+	def update
+		@book = Book.find(params[:id])
+		if @book.update(book_params)
+			flash[:notice] = "You have updated book successfully."
+			redirect_to book_path(@book)
+		else
+			render "books/edit"
+		end
+	end
+
+	def destroy
+		book = Book.find(params[:id])
+		book.destroy
+		redirect_to books_path
+	end
+
+	def edit
+		@book = Book.find(params[:id])
 	end
 
 	private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "books#top"
   get "about" => "books#about"
-  resources :books, only: [:new, :create, :index, :show]
+  resources :books
   resources :users, only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
